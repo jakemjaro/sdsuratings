d9274e7df032ed5efdb5c6c1748701df4322882c
Jake <jakemjaro@gmail.com>
2024-08-03 17:27:15 -0700

    Didn't add anything, just cleaned up the script.js file

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -8,20 +8,3 @@ document.querySelector('#search-form').addEventListener('focusout', clearDropdow
 function removeRatingForm() {
     document.querySelector('.rating-form-container').remove();
 }
-
-//const ratingFormContainer = document.querySelector('.rating-form-container');
-//const ratingForm = document.querySelector('.rating-form');
-//ratingFormContainer.addEventListener("click", () => {
-//    ratingFormContainer.style.display = "hidden";
-//    ratingForm.style.display = "hidden";
-//});
-
-//function attachListeners() {
-//    document.querySelector('.rating-form').addEventListener('submit', () => {
-//        this.parentNode.remove();
-//    });
-//
-//    document.querySelector('rating-form').addEventListener('reset', () => {
-//        this.parentNode.remove();
-//    });
-//}
