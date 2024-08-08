2002dbf8c363b3a2ebb63108ef0712225d2460f3
Jake <jakemjaro@gmail.com>
2024-08-08 00:00:41 -0700

    Implemented removing the rating form after it is submitted

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -10,3 +10,7 @@ document.querySelector('#search-form').addEventListener('focusout', clearDropdow
 function removeRatingForm() {
     document.querySelector('.rating-form-container').remove();
 }
+
+function removeRatingFormDelayed() {
+    setTimeout(removeRatingForm, 200);
+}
